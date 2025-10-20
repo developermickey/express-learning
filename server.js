5c0a7ee545ec22c9d1712badc33b327dcd1347f5
developermickey <mukesh.quazma@gmail.com>
2025-10-20 12:31:35 +0530

    basic routes and ejs template set

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+const indexRouter = require("./routes/index");
+const path = require("path");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
